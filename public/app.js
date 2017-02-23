7271e45f4154dabf22c6e544795e7ad7a638136f
Patrick Ford <patrickford@msn.com>
2017-02-23 10:40:00 -0800

    Added public directory

---
+++ public/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  $("#name").submit(function(event) {
+    event.preventDefault();
+
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var person = {
+      first: firstName,
+      last: lastName
+    }
+
+    $.ajax({     
+      url: "http://localhost:8080/name",
+      method: "POST",
+      data: JSON.stringify(person),
+      contentType: "application/json",
+      dataType: "json",
+      success: function(result) {
+        var output = "The name is " + result.name
+        $("#results").append(output);
+      },
+      error: function(error) {
+        $("#results").append("Error")
+      }
+    });
+
+  });
+
+});
